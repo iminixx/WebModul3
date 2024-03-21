95bca2de6c81a5dcb845dac65ff3ecdb3d63d3ce
iminixx <akurezzaa@gmail.com>
2024-03-21 20:09:24 +0700

    first commit

---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Welcome to our website</h1>
+      <p>Explore and learn more about us!</p>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Contact us for any inquiries or feedback.</p>
+    </div>
+  );
+};
+
+export default Contact;
